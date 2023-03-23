3ff4d896525d91a678bb5f6c195914681089b2c0
tramnguyenhere <tramngoc1402@gmail.com>
2023-03-23 11:56:36 +0200

    Worked on cart toggle and empty cart

--- src/components/UI/Cart/Cart.tsx
+++ src/components/UI/Cart/Cart.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import './Cart.scss'
+import CancelIcon from '@mui/icons-material/Cancel';
+import Button from '../Button/Button'
+import { useDispatch } from 'react-redux';
+import { toggleCartUI } from '../../../redux/cart/cartUISlice';
+
+const EmptyCart = () => {
+  return (
+    <div className="cart--empty">
+      Ostokorisi on tyhjä.
+    </div>
+  )
+}
 
 const Cart = () => {
+  const dispatch = useDispatch()
+
+  const toggleCart = () => {
+    dispatch(toggleCartUI())
+  }
+
   return (
-    <div>Cart</div>
+    <div className='cart' >
+      <div onClick={toggleCart} className='cart-overlay'></div>
+      <Button label={<CancelIcon />} className='cart__button--close' onClick={toggleCart} />
+      <div className="cart-list">
+        <EmptyCart />
+      </div>
+    </div>
   )
 }
 
